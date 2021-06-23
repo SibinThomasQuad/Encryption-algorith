490df8ca500f3aa8df9b4b87c49922e699862f93
QU4D <sibin@icsoft.in>
2021-06-24 04:13:42 +0530

    Push after reverse pass bug fixing

--- crypto.py
+++ crypto.py
@@ -1,26 +1,32 @@
 def password_sum(password):
-    pass_code=0
+    pass_code=''
     for word in password:
         asci=ord(word)
-        pass_code=pass_code+asci
-        return pass_code
+        pass_code=str(pass_code)+str(asci)
+    return int(pass_code)
 def password_length(password):
     length=len(password)
     return length
 def decrypt(cypher,password):
     skelt=''
     cypher=int(cypher, 16)
-    pass_code=password_sum(password)
-    pass_length=password_length(password)
-    salt=pass_code*pass_length**2
-    string_list = str(cypher).split(str(salt))
-    while("" in string_list):
-        string_list.remove("")
-    #print(string_list)
-    for numbers in string_list:
-        decoded=chr(int(numbers) // int(pass_length) // int(pass_code) // int(salt))
-        skelt=skelt+str(decoded)
-    return skelt
+    try:
+        #line
+        pass_code=password_sum(password)
+        pass_length=password_length(password)
+        cypher=cypher // pass_length
+        salt=pass_code*pass_length**2
+        string_list = str(cypher).split(str(salt))
+        while("" in string_list):
+            #line
+            string_list.remove("")
+            #print(string_list)
+        for numbers in string_list:
+            decoded=chr(int(numbers) // int(pass_length) // int(pass_code) // int(salt))
+            skelt=skelt+str(decoded)
+        return skelt
+    except:
+        return "Invalid Key"
 def encrypt(string,password):
     #line
     skelt=''
@@ -31,4 +37,4 @@ def encrypt(string,password):
         #line
         asci=ord(word)*salt*pass_code*pass_length
         skelt=skelt+str(asci)+str(salt)
-    return hex(int(skelt))
+    return hex(int(skelt)*pass_length)
--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 import crypto
-hash=crypto.encrypt('sibinmdkm@gmail.com','Thomas')
+hash=crypto.encrypt('hai ruby this is the message that what im sending on the computer is why','locationspoofedbysame')
 #crypto.encrypt('text',password)
 print(hash)
-print(crypto.decrypt(hash,'Thomas'))
+print(crypto.decrypt(hash,'locationspoofedbysame'))
 #crypto.decrypt(cypher,password)
